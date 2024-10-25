a304097ef5b363cc05cfdf73cc422c0980ab01b2
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-10-25 09:07:18 +0900

    [feat] hover effect 적용

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -6,15 +6,19 @@ const Experience = () => {
           <h2 className="experience-section-education-title">Education.</h2>
           <div className="experience-section-education-item">
             <div className="experience-section-education-item-info">
-              <h3>상명대학교 스마트정보통신공학과</h3>
-              <p>2017.03 - 2023.02</p>
+              <div className="hover-effect title">
+                상명대학교 스마트정보통신공학과
+              </div>
+              <br />
+              <p className="hover-effect">2017.03 - 2023.02</p>
             </div>
             <div className="experience-section-education-item-info">
               <div className="experience-section-education-item-info-academy">
-                <h3>멋쟁이사자처럼 블록체인스쿨 3기</h3>
-                <h3 style={{ fontWeight: "300" }}>(Frontend / Blockchain)</h3>
+                <div className="hover-effect title">
+                  멋쟁이사자처럼 블록체인스쿨 3기 (Frontend / Blockchain)
+                </div>
               </div>
-              <p>2023.03 - 2023.07</p>
+              <p className="hover-effect">2023.03 - 2023.07</p>
             </div>
           </div>
         </section>
@@ -23,17 +27,18 @@ const Experience = () => {
           <div className="experience-section-work-item">
             <div className="experience-section-work-item-info">
               <div className="experience-section-work-item-info-link">
-                <h3>JEFF World</h3>
-                <h3 style={{ fontWeight: "300" }}>/ Frontend Engineer</h3>
+                <div className="hover-effect title">
+                  JEFF World / Frontend Engineer
+                </div>
                 <a
                   href="https://jeffworld.io/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  [JEFF World Website]
+                  <div className="hover-effect link">[JEFF World Website]</div>
                 </a>
               </div>
-              <p>2023.08 - Current</p>
+              <p className="hover-effect">2023.08 - Current</p>
             </div>
           </div>
         </section>
