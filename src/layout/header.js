073bef43dece942a5206fe67ae4569e50903f21c
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-10-25 17:05:34 +0900

    [feat] mobile 일 경우 헤더 배경 색 적용

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import { MENUS } from "../constant/menu";
 
 function Header({ onMenuClick, pageIndex }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
   return (
-    <div className="header">
+    <div className={`header${isMobile ? " mobile" : ""}`}>
       <div className="header-menu">
         {MENUS.map((menu, index) => {
           return (
